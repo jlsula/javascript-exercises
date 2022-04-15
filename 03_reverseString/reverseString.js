5420981895355b9fa85dcd23629970dcb6f683e4
jlsula <joelsdevs@gmail.com>
2022-04-15 14:12:47 +0300

    Exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  // Reversing string
+  // 1. split string ["H", "E", "L", "L", "0"]
+  // 2. reverse array ["O", "L", "L"; "E"; "H]
+  // 3. join all elements of array into a string "OLLEH"
+  return string.split("").reverse().join("");
 };
 
+reverseString("HELLO");
 // Do not edit below this line
 module.exports = reverseString;
