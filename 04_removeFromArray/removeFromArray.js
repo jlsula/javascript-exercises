eb1e432bd48422d3968a957914c4471d467c20cd
jlsula <joelsdevs@gmail.com>
2022-04-17 12:38:17 +0300

    remove from array function completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+  // First item in arguments is the array
+  var array = args[0];
 
+  // new array
+  var newArray = [];
+
+  // forEach to loop through the array
+  array.forEach((item) => {
+    // push elements into the array, unless its included in the arguments
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
