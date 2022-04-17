87c897031b706f9b342a8e7c18bf4ccf72960774
jlsula <joelsdevs@gmail.com>
2022-04-17 15:33:09 +0300

    sum all function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+  let sum = 0;
 
+  // If higher number is first, swap the locations
+  if (min > max) {
+    const tempMin = min;
+    min = max;
+    max = tempMin;
+  }
+  // If given number is negative, return error
+  if (min < 0 || max < 0) {
+    return "ERROR";
+  }
+
+  // If given parameter is not a integer, return error
+  if (!Number.isInteger(min) || !Number.isInteger(max)) {
+    return "ERROR";
+  }
+  for (let i = min; i < max + 1; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
