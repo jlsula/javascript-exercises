11a30fa8b1ff604786dd959fdf78910c4fb49271
jlsula <joelsdevs@gmail.com>
2022-04-17 16:18:48 +0300

    farenheit to celcius and vice versa convertor

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (faren) {
+  var result = Math.round((faren - 32) * (5 / 9) * 10) / 10;
+  return result;
 };
 
-const ctof = function() {
-
+const ctof = function (celcius) {
+  var result = Math.round((celcius * (9 / 5) + 32) * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
